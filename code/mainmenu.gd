07109350e3b972ac16cd392a7d2d11f315d5eaa3
Kubota-Productions <124377538+Kubota-Productions@users.noreply.github.com>
2025-08-21 19:54:52 +0900

    changebuttons

--- code/mainmenu.gd
+++ code/mainmenu.gd
@@ -1,10 +1,17 @@
 extends Control
 
-func _on_start_pressed() -> void:
+func _on_load_a_sheet_pressed() -> void:
 	get_parent()._switch_to(preload("res://scenes/main_scene.tscn"))
 
-func _on_options_pressed() -> void:
-	print("Options pressed")
+func _on_add_a_sheet_pressed() -> void:
+	pass # Replace with function body.
+	
+func _on_delete_a_sheet_pressed() -> void:
+	pass # Replace with function body.
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+
+	
