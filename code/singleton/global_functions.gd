4c174fbe1184504895c2a9b830240a82a5c753ba
Kubota-Productions <124377538+Kubota-Productions@users.noreply.github.com>
2025-08-21 23:48:21 +0900

    update
    -issues
    
    can't save when just changing the character name
    
    can't load character sheets
    
    no dynamic text size changing
    
    otherwise excellent

--- code/singleton/global_functions.gd
+++ code/singleton/global_functions.gd
@@ -9,13 +9,22 @@ func _ready() -> void:
 		var all_data :Array[String]
 		all_data = file.get_var()
 		data = all_data
-		
+
 func _update_data() -> void:
 	var file = FileAccess.open(str("user://"+player_name+"0savedata.json"),FileAccess.READ)
 	if file:
 		var all_data :Array[String]
 		all_data = file.get_var()
 		data = all_data
+		store_data_all()
+
+func store_data_all():
+			data.resize(100)
+			var file = FileAccess.open(str("user://"+player_name+"0savedata.json"),FileAccess.WRITE)
+			if file:
+				file.store_var(data)
+				file.close()
+				print("just_finished_editing")
 
 func store_data(index : int, string : String):
 			data.resize(100)
@@ -29,7 +38,7 @@ func store_data(index : int, string : String):
 func set_player_name(name : String):
 	player_name = name
 	_update_data()
-	
+
 func return_player_name():
 	return player_name
 	
