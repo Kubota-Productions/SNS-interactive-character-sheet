6bf46c67258b76e4507dc3d0ecf2153fec5a4088
Kubota-Productions <124377538+Kubota-Productions@users.noreply.github.com>
2025-08-31 00:13:30 +0900

    update

--- scenes/genotypedisplayer.gd
+++ scenes/genotypedisplayer.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var malesyllabub: Node3D = $SubViewportContainer/SubViewport/malesyllabub
 @onready var femalesyllabub: Node3D = $SubViewportContainer/SubViewport/femalesyllabub
 @onready var datadoll: Node3D = $SubViewportContainer/SubViewport/Datadoll
+@onready var cognitive: Node3D = $SubViewportContainer/SubViewport/Cognitive
 
 # Rotation speed in degrees per second
 @export var rotation_speed: float = 90.0  # degrees per second
@@ -16,9 +17,15 @@ func _process(delta: float) -> void:
 	# Rotate left if the left button is held
 	if rotating_left:
 		datadoll.rotate_z(deg_to_rad(rotation_speed * delta))
+		femalesyllabub.rotate_z(deg_to_rad(rotation_speed * delta))
+		malesyllabub.rotate_z(deg_to_rad(rotation_speed * delta))
+		cognitive.rotate_z(deg_to_rad(rotation_speed * delta))
 	# Rotate right if the right button is held
 	if rotating_right:
 		datadoll.rotate_z(deg_to_rad(-rotation_speed * delta))
+		femalesyllabub.rotate_z(deg_to_rad(-rotation_speed * delta))
+		malesyllabub.rotate_z(deg_to_rad(-rotation_speed * delta))
+		cognitive.rotate_z(deg_to_rad(-rotation_speed * delta))
 
 
 func _on_leftbutton_button_down() -> void:
